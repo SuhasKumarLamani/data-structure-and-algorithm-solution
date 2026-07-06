f2aebac828a17f1200853d02a01cc6501e7dd706
Suhas Kumar Lamani <suhasklamani.gmail.com>
2026-07-06 23:49:16 +0530

    Add LeetCode 1381 Design a Stack With Increment Operation solution

---
+++ Leetcode/Medium/1381_Design_a_Stack_With_Increment_Operation/README.md
@@ -0,0 +1,16 @@
+#1381. Design a Stack With Increment Operation
+
+##Difficulty
+
+Medium
+
+##Time Complexity
+
+- push() : O(1)
+- pop() : O(1)
+- increment() : O(min(k, n))
+
+##Space Complexity
+
+O(n)
+
---
+++ Leetcode/Medium/1381_Design_a_Stack_With_Increment_Operation/solution.cpp
@@ -0,0 +1,49 @@
+class CustomStack {
+public:
+    int maxSize;
+    vector <int> st;
+    
+    CustomStack(int maxSize) {
+        this->maxSize=maxSize;
+        
+    }
+    
+    void push(int x) {
+       if(st.size() != maxSize)
+       {
+            st.push_back(x);
+           
+       };
+      
+    }
+    
+    int pop() {
+        if(st.size() == 0)
+        {
+            return -1;
+        }
+        else
+        {
+            int  x= st.back();
+            st.pop_back();
+            return x;
+        }
+
+    }
+    
+    void increment(int k, int val) {
+        for(int i=0;i<min(k,(int)st.size());i++)
+        {
+            st[i] += val;
+        }
+    }
+    
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
